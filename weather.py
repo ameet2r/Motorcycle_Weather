1547f500233ef08f861adfc4a561cccc66529600
Ameet Toor <Ameet_Toor@Hotmail.com>
2025-07-31 17:28:29 -0700

    Add general TODOs to main file to get an idea of next steps

--- weather.py
+++ weather.py
@@ -1,6 +1,14 @@
 def main():
     print("Motorcycle Weather")
+    # TODO get directions between two routes
 
+    # TODO coordinates for the many locations on the given route. Maybe only the major cities? 
+    
+    # TODO get forcast for each set of coordinates. Ideally at the exact time that I will reach each set of coordinates. Maybe using the hourly forecast if that is available?
+
+    # TODO display forecast for each set of coordinates
+
+    # TODO suggest gear that I will need.
 
 if __name__ == "__main__":
     main()
